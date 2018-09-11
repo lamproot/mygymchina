dcb77f927cc6b6608feae7406e77b4a64e335cdb
quanboxiong <quanboxiong@6clue.com>
2018-09-11 21:08:22 +0800

    gengxin

--- php/index.php
+++ php/index.php
@@ -3,17 +3,17 @@ $mysqli = mysqli_init();
 $mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 2);//设置超时时间
 $mysqli->real_connect('telegram.ck7uunebefgc.us-east-2.rds.amazonaws.com', 'telegram', 'lamproot', 'telegram');
 $data = $_POST;
-//$mysqli->query("SET NAMES utf8");
-//$sql = "insert into mygymchina(Box_name,Box_birthday,Box_isvip,Box_mobile,time) values ('{$data['Box_name']}','{$data['Box_birthday']}',{$data['Box_isvip']},'{$data['Box_mobile']}',".time().")";
-//$mysqli->query($sql);
-$result = $mysqli->query("select * from mygymchina");
-$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
-//echo json_encode($row);exit;
-foreach ($row as $key => $value) {
-	$mysqli->query("SET NAMES utf8");
-
-	$mysqli->query("update mygymchina set Box_name = '{$value['Box_name']}' where id = {$value['id']}");
-}
+$mysqli->query("SET NAMES utf8");
+$sql = "insert into mygymchina(Box_name,Box_birthday,Box_isvip,Box_mobile,time) values ('{$data['Box_name']}','{$data['Box_birthday']}',{$data['Box_isvip']},'{$data['Box_mobile']}',".time().")";
+$mysqli->query($sql);
+// $result = $mysqli->query("select * from mygymchina");
+// $row=mysqli_fetch_all($result,MYSQLI_ASSOC);
+// //echo json_encode($row);exit;
+// foreach ($row as $key => $value) {
+// 	$mysqli->query("SET NAMES utf8");
+//
+// 	$mysqli->query("update mygymchina set Box_name = '{$value['Box_name']}' where id = {$value['id']}");
+// }
 // $rst = $mysqli->query($sql);
 echo json_encode(array("code" => "200", "msg" => "添加成功"));
 // mysql_connect("localhost","root","root");//连接MySQL
